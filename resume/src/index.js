150415333bd06b31f8efe47b2afad2efc66ef724
Vincent Vitale <vincent.vitale87@gmail.com>
2020-06-14 18:02:38 -0700

    removed unnecessary files

--- resume/src/index.js
+++ resume/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "./services/serviceHelper";
 import "bootstrap/dist/css/bootstrap.css";
 import "react-toastify/dist/ReactToastify.css";
 import "sweetalert2/dist/sweetalert2.min.css";
