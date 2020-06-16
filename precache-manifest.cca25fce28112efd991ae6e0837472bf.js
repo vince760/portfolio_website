1e8303e35f7d6ddb2ec566d767d1698f4ff887fa
Vincent Vitale <vincent.vitale87@gmail.com>
2020-06-15 22:06:06 -0700

    Updates

---
+++ precache-manifest.cca25fce28112efd991ae6e0837472bf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611a2a7813cc39ba463b59ab1ca32eeb",
+    "url": "/portfolio_website/index.html"
+  },
+  {
+    "revision": "7ee3b415b40a56390822",
+    "url": "/portfolio_website/static/css/2.af5531ac.chunk.css"
+  },
+  {
+    "revision": "bb53e4f752c68086ecd8",
+    "url": "/portfolio_website/static/css/main.c5975480.chunk.css"
+  },
+  {
+    "revision": "7ee3b415b40a56390822",
+    "url": "/portfolio_website/static/js/2.7e3a43ef.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio_website/static/js/2.7e3a43ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb53e4f752c68086ecd8",
+    "url": "/portfolio_website/static/js/main.787517e1.chunk.js"
+  },
+  {
+    "revision": "c3d89cccf0a2955331f0",
+    "url": "/portfolio_website/static/js/runtime-main.a9dc6194.js"
+  },
+  {
+    "revision": "2ef4447de43c8f338d494a3fe58e4b82",
+    "url": "/portfolio_website/static/media/forceTracker.2ef4447d.jpg"
+  },
+  {
+    "revision": "6fb8cd7e97195c554da3eab3c1ee5dea",
+    "url": "/portfolio_website/static/media/header-background.6fb8cd7e.jpg"
+  },
+  {
+    "revision": "0067164308914781e20805b214ad2568",
+    "url": "/portfolio_website/static/media/profilepic.00671643.jpg"
+  }
+]);
